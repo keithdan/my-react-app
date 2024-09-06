9d20be6e1daa83fd96366a699b23bca6a194f77a
keithdan <43763700+keithdan@users.noreply.github.com>
2024-09-06 01:08:29 -0400

    Add ApiData component to fetch and display post titles
    * **ApiData.js**
      - Create `ApiData` component
      - Use `useEffect` to fetch data from API
      - Store fetched data in state variable
      - Render list of post titles
    
    * **App.js**
      - Import `ApiData` component
      - Render `ApiData` component within `App` component's main structure

---
+++ src/ApiData.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
+const ApiData = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Post Titles</h1>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ApiData;
